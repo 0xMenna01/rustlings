ae9362ea98b4c55d92d65ef34742faf8e8bf2224
0xMenna01 <emanuele.cryptdev@gmail.com>
2023-03-29 19:10:26 +0200

    quiz1 excercise done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_boughts: i32) -> i32 {
+    let mut apple_price = 2;
+    if (apple_boughts > 40) {
+        apple_price = 1;
+    }
+    let price = apple_boughts * apple_price;
+    price
+}
 
 // Don't modify this function!
 #[test]
